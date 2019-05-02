a2106fef4d64f0c0d077de082c5889525265de28
YJin <yjin2@uoregon.edu>
2019-05-01 20:43:27 -0700

    player movement update

--- Dungeon And Brave/Assets/Scripts/player/PlayerMovement.cs
+++ Dungeon And Brave/Assets/Scripts/player/PlayerMovement.cs
@@ -8,38 +8,63 @@ public class PlayerMovement : MonoBehaviour
     Vector3 move;
     Rigidbody playerRigidbody;
     Animator anim;
+    int floor;
+    float camRayL = 100f;
 
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         speed = speed * Time.deltaTime;
         playerRigidbody = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+        floor = LayerMask.GetMask("Floor");
     }
 
     // FixedUpdate is called once per every physic update
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Move(x, z);
+        Turning();
         Anmi(x, z);
     }
 
-    void Move(float x, float z)
+    private void Move(float x, float z)
     {
         //according to input come up with vector3
-        move.Set(x, 0f, z);
+        move.Set(x, 0f, z); 
         //normalize
         move = move.normalized;
+        //transform.position = Vector3.Lerp(transform.position, transform.position + move, speed);
         playerRigidbody.MovePosition(transform.position + move * speed);
     }
 
-    void Anmi(float x, float z)
+    private void Turning()
     {
-        bool runing = x != 0f || z != 0f;
+        Ray camR = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+
+        //Physics.Raycast(V3: origin, V3: driction, floaf: maxDis, int: layerMask,QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
+        //return True if the ray intersects with a Collider, otherwise false.
+        if (Physics.Raycast(camR, out floorHit, camRayL, floor))
+        {
+            Vector3 playToM = floorHit.point - transform.position;
+            playToM.y = 0f;
+
+            Quaternion rotation = Quaternion.LookRotation(playToM);
+            playerRigidbody.MoveRotation(rotation);
+        }
+
+
+    }
+
+
+    private void Anmi(float x, float z)
+    {
+        bool runing = (x != 0f) || (z != 0f);
         anim.SetBool("isRun", runing);
     }
 
