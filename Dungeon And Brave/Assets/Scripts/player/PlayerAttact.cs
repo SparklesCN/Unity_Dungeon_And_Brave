088bcc6a0d18c73e97c8e983487e38726f69764d
YJin <yjin2@uoregon.edu>
2019-05-02 15:01:34 -0700

    Player Attact

--- Dungeon And Brave/Assets/Scripts/player/PlayerAttact.cs
+++ Dungeon And Brave/Assets/Scripts/player/PlayerAttact.cs
@@ -9,18 +9,25 @@ public class PlayerAttact : MonoBehaviour
     float myTime = 0f;
     float nextFire = 0.5f;
     public float attactGap = 1f;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         myTime += Time.deltaTime;
+        bool check = Input.GetButton("Fire1") && myTime > nextFire;
 
-        if (Input.GetButton("Fire1") && myTime > nextFire){
+        if (check){
             nextFire += attactGap;
             //Instantiate(Object original, Vector3 position, Quaternion rotation)
             GameObject copy_attact = Instantiate(attact, spellPos.position, spellPos.rotation) as GameObject;
-
-
         }
+        anim.SetBool("isAttack", check);
     }
+
 }
