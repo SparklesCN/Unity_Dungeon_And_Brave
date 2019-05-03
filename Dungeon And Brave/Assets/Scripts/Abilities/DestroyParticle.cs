3575129d9b7d32b338bf74f881cfe32938308b23
SparklesCN <nathanquan.us@gmail.com>
2019-05-02 19:37:00 -0700

    save for later

---
+++ Dungeon And Brave/Assets/Scripts/Abilities/DestroyParticle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticle : MonoBehaviour
+{
+    public float lifetime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        Destroy(gameObject);
+    }
+}
