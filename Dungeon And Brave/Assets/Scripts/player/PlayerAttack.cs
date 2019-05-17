e16d208118f57d93847f18cec1289b1402489952
Sparkles <nathanquan.us@gmail.com>
2019-05-17 09:56:15 -0700

    added logic on Exp Bar

--- Dungeon And Brave/Assets/Scripts/player/PlayerAttack.cs
+++ Dungeon And Brave/Assets/Scripts/player/PlayerAttack.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAttack : MonoBehaviour
 {
     public GameObject attact;
     public Transform spellPos;
+
+    Text expText, levelText;
+    Slider levelSlider;
     float myTime = 0f;
     public float attackGap = 1f;
     Animator anim;
+    float target = 280;
 
     int level = 1;
-    public int curEXP = 0;
+    int curEXP = 100;
     public int constantOFlevel = 1;
     //source:https://www.diablowiki.net/Experience_level_chart
     IList<int> expList = new List<int> {0, 280, 2700, 4500, 6600, 9000, 11700, 14000, 16500, 19200};
@@ -20,12 +25,24 @@ public class PlayerAttack : MonoBehaviour
     private void Start()
     {
         anim = GetComponent<Animator>();
+        levelSlider = GameObject.Find("LevelSlider").GetComponent<Slider>();
+        expText = GameObject.Find("ExpText").GetComponent<Text>();
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Attack();
+        expText.text = curEXP.ToString() + "/" + target.ToString();
+        levelText.text = "Level: " + level.ToString();
+        if (curEXP == 0)
+        {
+            curEXP += 1;
+        }
+        Debug.Log(curEXP);
+        levelSlider.value = curEXP / target * 100;
+
     }
     void Attack()
     {
@@ -43,7 +60,7 @@ public class PlayerAttack : MonoBehaviour
 
     public void levelup(int enemyExp)
     {
-        float target = expList[level-1];
+        target = expList[level-1];
         curEXP += enemyExp;
         if (curEXP >= target)
         {
