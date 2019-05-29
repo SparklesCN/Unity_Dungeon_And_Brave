c476e008b8cb530beb5ee7f2df96993c9d51ef0c
Sparkles <nathanquan.us@gmail.com>
2019-05-29 12:55:57 -0700

    added login Scene without button logic

---
+++ Dungeon And Brave/Assets/LoginStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button startButton = this.GetComponent<Button>();
+        startButton.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        Debug.Log("Clicked! Start!");
+    }
+}
