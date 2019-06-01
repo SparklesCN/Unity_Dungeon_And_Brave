42bbc0c5fa255b259748dc2adabd1beb064f2067
Sparkles <nathanquan.us@gmail.com>
2019-05-31 20:00:21 -0700

    ready for beta tag

---
+++ Dungeon And Brave/Assets/DataInitial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataInitial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+       
+        PlayerPrefs.SetInt("HP", 100);
+        PlayerPrefs.SetInt("MP", 100);
+        PlayerPrefs.SetInt("EXP", 0);
+        PlayerPrefs.SetInt("LV", 1);
+        PlayerPrefs.SetInt("SceneNum", 1);
+       
+    }
+
+}
--- Dungeon And Brave/Assets/Scripts/DataControlor.cs
+++ Dungeon And Brave/Assets/Scripts/DataControlor.cs
@@ -5,12 +5,5 @@ using UnityEngine;
 public class DataControlor : MonoBehaviour
 {
     public int nextScene, formerScene;
-    private void Start()
-    {
-        PlayerPrefs.SetInt("HP", 100);
-        PlayerPrefs.SetInt("MP", 100);
-        PlayerPrefs.SetInt("EXP", 0);
-        PlayerPrefs.SetInt("LV", 1);
-        PlayerPrefs.SetInt("SceneNum", 1);
-    }
+
 }
